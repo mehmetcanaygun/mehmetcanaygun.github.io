4cafd3f5a25bc484bf51e32a5714008a1dd6f0ba
Can <mca199667@gmail.com>
2019-12-24 15:03:57 +0300

    RecipeApp added

---
+++ projects/recipeapp/precache-manifest.e738e8c65b12c70c998d5328d1bd84b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66c23c4d4a034ea8272e32b0e59304e5",
+    "url": "/projects/recipeapp/index.html"
+  },
+  {
+    "revision": "c727676599f0a484174f",
+    "url": "/projects/recipeapp/static/css/main.e6d29a9f.chunk.css"
+  },
+  {
+    "revision": "4b94141b99ae7afd25c0",
+    "url": "/projects/recipeapp/static/js/2.e57fb74b.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/projects/recipeapp/static/js/2.e57fb74b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c727676599f0a484174f",
+    "url": "/projects/recipeapp/static/js/main.be4d33fa.chunk.js"
+  },
+  {
+    "revision": "cad0dc48bbbf037f4e47",
+    "url": "/projects/recipeapp/static/js/runtime-main.14c01516.js"
+  }
+]);
