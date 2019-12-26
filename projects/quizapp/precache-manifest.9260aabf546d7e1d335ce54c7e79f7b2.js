1a7938394e6817df80a865d72372aaff5865cd1d
Can <mca199667@gmail.com>
2019-12-26 23:05:35 +0300

    QuizApp added

---
+++ projects/quizapp/precache-manifest.9260aabf546d7e1d335ce54c7e79f7b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b575cae840c6b30601aaf644d636442c",
+    "url": "/projects/quizapp/index.html"
+  },
+  {
+    "revision": "b3372168d7492331a610",
+    "url": "/projects/quizapp/static/css/main.b1fbcac2.chunk.css"
+  },
+  {
+    "revision": "855c732bc1fca2aa220d",
+    "url": "/projects/quizapp/static/js/2.6c080cc6.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/projects/quizapp/static/js/2.6c080cc6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b3372168d7492331a610",
+    "url": "/projects/quizapp/static/js/main.87736ae1.chunk.js"
+  },
+  {
+    "revision": "7b608215a6c9eaa70451",
+    "url": "/projects/quizapp/static/js/runtime-main.8411e126.js"
+  }
+]);
