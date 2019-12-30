443133395798fa599257cc6ec57112bfcbe1252c
Can <mca199667@gmail.com>
2019-12-30 17:57:01 +0300

    Portfolio updated

---
+++ precache-manifest.f759bce008765d11e6b77c98a94ecfc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f45c34f77f194c71cfb881639915082",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f79db2cc01402a6a3f8a",
+    "url": "/static/css/main.9111cff4.chunk.css"
+  },
+  {
+    "revision": "af0dfddfb7a27a7d3879",
+    "url": "/static/js/2.c828e42e.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/static/js/2.c828e42e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f79db2cc01402a6a3f8a",
+    "url": "/static/js/main.1ef3b538.chunk.js"
+  },
+  {
+    "revision": "fa586c66576130f10b91",
+    "url": "/static/js/runtime-main.e7014683.js"
+  }
+]);
